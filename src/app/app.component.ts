7d7a7647fb130a0d1b6340c5c3b41c83c6b849ab
Jairo Martinez <jamartinez@iseguros.com>
2018-05-22 09:51:49 -0500

    Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title:string = 'PlatziSquare';
+  lugares:any = [
+    {plan: 'pagado', cercania:1, distancia: 1,active: true,nombre:'Florería la Gardenia'},
+    {plan: 'gratuito', cercania:1, distancia: 1.8,active: true,nombre:'Donas las pasaditas'},
+    {plan: 'gratuito', cercania:2, distancia: 5,active: true,nombre:'Veterinaria huellitas felices'},
+    {plan: 'gratuito', cercania:3, distancia: 10,active: false,nombre:'Sushi Sushiroll'},
+    {plan: 'pagado', cercania:3, distancia: 35,active: true,nombre:'Hotel la Gracia'},
+    {plan: 'gratuito', cercania:3, distancia: 120,active: false,nombre:'Zapatería el Clavo'},
+  ];
+  lat:number=8.9856285;
+  lng:number=-79.5124686;
+  constructor(){
+    
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from '@agm/core';
+import { ResaltarDirective } from './directives/resaltar.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDWtfHIaxmVFz5KjWRNUflBCAmoQVsX0QU'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
